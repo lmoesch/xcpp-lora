adc66d931d4afdb1d4a53f994f0e078da88e2ec5
Lucas <lucas.moesch@rwth-aachen.de>
2018-01-20 10:05:31 +0100

    added Operation Mode Register

--- sx127x.hpp
+++ sx127x.hpp
@@ -1,1 +1,9 @@
+// ----------------------------------------------------------------------------
+/* Copyright (c) 2018, Lucas Mösch
+ * All Rights Reserved.
+ *
+ * The file is part of the xpcc library and is released under the 3-clause BSD
+ * license. See the file `LICENSE` for the full license governing this code.
+ */
+// ----------------------------------------------------------------------------
 
